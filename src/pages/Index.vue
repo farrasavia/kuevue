7c4b15eeb1ce7c110e039a0cae6be443f637dbee
farrasavia <farras251096@gmail.com>
2019-10-10 11:21:37 +0700

    finish

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page class="flex flex-center">
     <button @click="clearMessage">Clear</button>
-    <input v-model="message" @keyup="handleKeyup"/>
+    <input v-model="message" @keyup="handleKeyup" @keydown="handleKeydown"/>
     <h1>{{message}}</h1>
     <img alt="Quasar logo" src="~assets/BL.jpg">
   </q-page>
@@ -19,12 +19,12 @@ export default {
     clearMessage(){
       this.message=''
     },
-    handleKeyup(e){
+    handleKeydown(e){
       if (e.keyCode == 27){
         this.message='ESC'
       }
     },
-    handlekeyDown(e){
+    handleKeyup(e){
       if (e.keyCode==13){
         this.message='ENTER'
       }
